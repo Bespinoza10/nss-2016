6b8a668512f34e6a3fb0754f62f543bfbbcac512
Brandon Espinoza <espinozab100@gmail.com>
2016-04-14 14:16:42 -0500

    Added images for part time page and finished part time page

---
+++ app/controllers/part_time_bootcamp_controller.rb
@@ -0,0 +1,7 @@
+class PartTimeBootcampController < ApplicationController
+
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
   resources :full_time_bootcamp
+  resources :part_time_bootcamp
 
   end
