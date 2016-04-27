202d1e89f4b4c3721f1d8e89c7fc8ce6cc303af7
Brandon Espinoza <espinozab100@gmail.com>
2016-04-27 13:36:10 -0500

    Created Programs Controller and merged all programs to the new controller.

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,35 @@
+class ProgramsController < ApplicationController
+
+  def full_time
+    
+  end
+
+  def part_time
+    
+  end
+
+  def professional_development
+    
+  end
+
+  def front_end
+    
+  end
+
+  def ruby
+    
+  end
+
+  def mean
+    
+  end
+
+  def csharp
+    
+  end
+
+  def ios_development
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  get 'full_time_bootcamp' => 'full_time_bootcamp#index'
-  get 'full_time_bootcamp/ruby' => 'full_time_bootcamp#ruby'
-  get 'full_time_bootcamp/mean' => 'full_time_bootcamp#mean'
-  get 'full_time_bootcamp/csharp' => 'full_time_bootcamp#csharp'
-  get 'full_time_bootcamp/front_end' => 'full_time_bootcamp#front_end'
-  resources :part_time_bootcamp
-  get 'professional_development_bootcamp' => 'professional_development_bootcamp#index'
-  get 'professional_development_bootcamp/ios_development' => 'professional_development_bootcamp#ios_development'
+  get 'programs/full_time' => 'programs#full_time'
+  get 'programs/part_time' => 'programs#part_time'
+  get 'programs/professional_development' => 'programs#professional_development'
+  get 'programs/front_end' => 'programs#front_end'
 
-  resources :front_end_development
+  get 'programs/ruby' => 'programs#ruby'
+  get 'programs/mean' => 'programs#mean'
+  get 'programs/csharp' => 'programs#csharp'
+
+  resources :part_time_bootcamp
+  get 'programs/ios_development' => 'programs#ios_development'
+
   resources :about
   end
