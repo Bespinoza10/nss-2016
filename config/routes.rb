4e6d3e86b50af9f295f65018478c32332f943222
Brandon Espinoza <espinozab100@gmail.com>
2016-04-28 14:25:49 -0500

    Created UI/UX page WIP and configured all routes to use dashes instead of underscores

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  get 'programs/full_time' => 'programs#full_time'
-  get 'programs/part_time' => 'programs#part_time'
-  get 'programs/professional_development' => 'programs#professional_development'
-  get 'programs/front_end' => 'programs#front_end'
+  get 'programs/full_time' => 'programs#full_time', :path => 'programs/full-time'
+  get 'programs/part_time' => 'programs#part_time', :path => 'programs/part-time'
+  get 'programs/professional_development' => 'programs#professional_development', :path => 'programs/professional-development'
+  get 'programs/front_end' => 'programs#front_end', :path => 'programs/front-end'
 
   get 'programs/ruby' => 'programs#ruby'
   get 'programs/mean' => 'programs#mean'
   get 'programs/csharp' => 'programs#csharp'
   get 'programs/python' => 'programs#python'
-  get 'programs/ios_development' => 'programs#ios_development'
+  get 'programs/ios_development' => 'programs#ios_development', :path => 'programs/ios-development'
+  get 'programs/ui_ux' => 'programs#ui-ux-design', :path => 'programs/ui-ux-design'
 
   resources :about
   end
